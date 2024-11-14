54a2e5273b2513423c85ff747afcf4ca4b6ea43a
Rowan-James Tran <rowanjames.tran@gmail.com>
2024-11-13 22:56:14 -0800

    h60: add qwerty layer

--- keyboards/hineybush/h60/keymaps/rowantran/keymap.c
+++ keyboards/hineybush/h60/keymaps/rowantran/keymap.c
@@ -7,7 +7,8 @@
 enum layer_names {
     _BASE,
     _FN,
-    _ARROWS
+    _ARROWS,
+    _QWERTY
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -21,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_LALT, KC_LWIN,                            KC_SPC,                             KC_RWIN,  KC_RALT),
 
     [_FN] = LAYOUT_60_hhkb(
-    TG(_ARROWS), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     KC_F13, KC_PAUS,
+    TG(_ARROWS), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,     KC_F12,     TG(_QWERTY), KC_PAUS,
     KC_TRNS,     KC_MUTE, KC_VOLD, KC_VOLU, KC_HOME, KC_PGUP, KC_INS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_UP,      KC_TRNS,    KC_DEL,
     KC_TRNS,     KC_MPLY, KC_MPRV, KC_MNXT, KC_END,  KC_PGDN, KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_RGHT,    KC_TRNS,
     KC_TRNS,     KC_PSCR, KC_SCRL, KC_PAUS, KC_BRID, KC_BRIU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOWN,  KC_TRNS,    KC_TRNS,
@@ -32,5 +33,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4,   KC_P5,   KC_P6,   KC_UP,    KC_TRNS,    KC_TRNS,    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1,   KC_P2,   KC_P3,   KC_DOWN,  KC_RGHT,    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P0,   KC_PDOT, KC_PENT, KC_TRNS,  KC_TRNS,    KC_TRNS,
+             KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS,  KC_TRNS),
+
+    [_QWERTY] = LAYOUT_60_hhkb(
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,
+    KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,     KC_TRNS,    KC_TRNS,    KC_TRNS,
+    KC_TRNS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_TRNS,    KC_TRNS,
+    KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,    KC_TRNS,
              KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS,  KC_TRNS)
 };
